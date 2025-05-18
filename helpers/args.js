12f0ec04eb31ebee5a31691c66f7fdd2c68350ac
radjabov_sherzod <155184029+rajabovsherzod@users.noreply.github.com>
2025-05-18 11:43:14 +0500

    project started

---
+++ helpers/args.js
@@ -0,0 +1,19 @@
+const getArgs = args =>{
+    const res = {}
+    const [executer, file, ...rest] = args
+    rest.forEach((value, idx, arr) => {
+        if(value.charAt(0) == '-'){
+            if(idx == arr.length-1){
+                res[value.substring(1)] = true
+            }else if(arr[idx+1].charAt(0) != '-'){
+                res[value.substring(1)] = arr[idx+1]
+            }else{
+                res[value.substring(1)] = true
+            }
+        }
+    })
+
+    return res
+}
+
+module.exports = getArgs
---
+++ index.js
@@ -0,0 +1,20 @@
+const getArgs = require('./helpers/args');
+
+const startCLI = () => {
+    const args = getArgs(process.argv);
+    if(args.h){
+
+    }
+    if(args.s){
+
+    }
+    if(args.t){
+
+    }
+
+    // result
+}
+
+startCLI();
+
+
