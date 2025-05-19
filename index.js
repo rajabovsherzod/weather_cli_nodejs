e5fc824a3268741c4ade8e296bcc07b03ad424cc
radjabov_sherzod <155184029+rajabovsherzod@users.noreply.github.com>
2025-05-19 12:20:03 +0500

    save city

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import getArgs from './helpers/args.js';
 import { printError, printSuccess, printHelp } from './services/log.service.js';
-import {TOKEN_DICTIONARY, saveKeyValue} from './services/storage.service.js';
+import {TOKEN_DICTIONARY, saveKeyValue, getKeyValue} from './services/storage.service.js';
 import { getWeather } from './services/api.service.js';
 
 const saveToken = async token => {
@@ -16,19 +16,50 @@ const saveToken = async token => {
     }
 }
 
+
+const saveCity = async city => {
+    if(!city.length){
+        printError("City doesn't exist")
+        return
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city)
+        printSuccess('City was saved')
+    } catch (error) {
+        printError('Error saving city:', error.message)
+    }
+}
+
+const getForcast = async () => {
+    try {
+        const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city)
+        const response = await getWeather(city);
+        console.log(response);
+    } catch (error) {
+        if(error?.response?.status == 404){
+            printError('City not found')
+        }else if(error?.response?.status == 401){
+            printError('Token is invalid')
+        }else{
+            printError('Error fetching weather:', error.message)
+        }
+    }
+}
+
 const startCLI = () => {
     const args = getArgs(process.argv);
     console.log(args);
     if(args.h){
-        printHelp()
+      return  printHelp()
     }
     if(args.s){
-
+       return saveCity(args.s)
     }
     if(args.t){
-        saveToken(args.t)
+      return saveToken(args.t)
     }
-    getWeather('London');
+
+    getForcast();
     
 }
 
--- services/api.service.js
+++ services/api.service.js
@@ -3,12 +3,11 @@ import https from 'https';
 import { TOKEN_DICTIONARY, getKeyValue } from './storage.service.js';
 
 const getWeather = async city => {
-    const token = await getKeyValue(TOKEN_DICTIONARY.token)
+    const token = process.env.TOKEN ?? (await getKeyValue(TOKEN_DICTIONARY.token))
     //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
     if(!token){
         throw new Error("API doesn't exist, use -t [API_KEY]")
     }
-    console.log(token);
 
     const {data} = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
         params: {
