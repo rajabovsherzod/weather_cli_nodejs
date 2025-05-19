ff6f35a672aa7b7fc3a79392a14b08d0d70af42b
radjabov_sherzod <155184029+rajabovsherzod@users.noreply.github.com>
2025-05-19 11:27:05 +0500

    fix bug AXIOS

--- services/api.service.js
+++ services/api.service.js
@@ -1,27 +1,26 @@
+import axios from 'axios';
 import https from 'https';
-import { TOKEN_DICTIONARY } from './storage.service.js';
+import { TOKEN_DICTIONARY, getKeyValue } from './storage.service.js';
 
 const getWeather = async city => {
-    const token = TOKEN_DICTIONARY.token
-    //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+    const token = await getKeyValue(TOKEN_DICTIONARY.token)
+    //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
     if(!token){
         throw new Error("API doesn't exist, use -t [API_KEY]")
     }
-    const url = new URL('https://api.openweathermap.org/data/2.5/weather')
-    url.searchParams.append('q', city)
-    url.searchParams.append('appid', token)
-    url.searchParams.append('lang', 'en')
-    url.searchParams.append('units', 'metric')
+    console.log(token);
 
-    https.get(url, (res) => {
-        let data = ''
-        res.on('data', chunk => {
-            data+=chunk
-        })
-        res.on('end', () => {
-            console.log(res)
-        })
+    const {data} = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+            q: city,
+            appid: token,
+            lang: 'en',
+            units: 'metric',
+        }
     })
+
+    return data
 }
+
 
 export {getWeather}
